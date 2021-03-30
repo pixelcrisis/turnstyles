6cebb6c718ea346ea2fafe3e72db87bf5d0cda62
12px <d@12px.io>
2021-03-30 10:58:49 -0600

    Add tabs to options panel.

--- scripts/layout.js
+++ scripts/layout.js
@@ -2,15 +2,23 @@
 
 module.exports = tS => {
 
-  let version = require('../package.json').version
+  const menu = '.ts_toggle'
+  const pane = '#ts_panel, #ts_window'
+  const tabs = '.opts_tab, .chat_tab, .ding_tab'
+  const opts = '#ts_panel input, #ts_window input, #ts_window select'
+  const vers = require('../package.json').version
 
   tS.prototype.buildPanel = function () {
     $('.header-bar').append(layout(this))
 
-    let toggled = $('#ts_panel, #ts_window')
-    let changed = $('#ts_panel input, #ts_window input, #ts_window select')
-    $('.ts_toggle').on('click', () => toggled.toggleClass('active'))
-    changed.on('change', this.saveConfig.bind(this))
+    $(tabs).on('click', onTab)
+    $(menu).on('click', () => $(pane).toggleClass('active'))
+    $(opts).on('change', this.saveConfig.bind(this))
+  }
+
+  const onTab = e => {
+    $(tabs).removeClass('active')
+    $(`.${e.currentTarget.className}`).addClass('active')
   }
 
   const layout = self => `
@@ -25,31 +33,37 @@ module.exports = tS => {
       <h3 class="ts_toggle">☰ turnStyles</h3>
       <label> ${select(self, 'theme')} </label>
       <label> ${select(self, 'style')} </label>
-      <div>
-        <h5>Options</h5>
+      <div id="ts_tabs">
+        <div class="opts_tab active">Options</div>
+        <div class="chat_tab">In Chat</div>
+        <div class="ding_tab">Notifications</div>
+      </div>
+      <div class="opts_tab active">
         <label> ${toggle(self, 'autobop')} Autobop </label>
         <label> ${toggle(self, 'has_vol')} Control Volume </label>
+        <br>
         <label> ${toggle(self, 'nextdj')} Next DJ Spot </label>
+        <h5>For Queues:</h5>
         <label> ${toggle(self, 'pingdj')} Next DJ On Ping </label>
       </div>
-      <div>
-        <h5>Post To Chat</h5>
+      <div class="chat_tab">
         <label>${toggle(self, 'chat_stat')} Song Stats</label>
         <label>${toggle(self, 'chat_snag')} User Snags</label>
         <label>${toggle(self, 'chat_join')} User Joins</label>
         <label>${toggle(self, 'chat_left')} User Leaves</label>
       </div>
-      <div>
-        <h5>Desktop Notifications</h5>
+      <div class="ding_tab">
         <label>${toggle(self, 'ping_pm')} On DMs</label>
         <label>${toggle(self, 'ping_chat')} On Mentions</label>
         <label>${toggle(self, 'ping_song')} On New Songs</label>
       </div>
       <div class="ts_credits">
         <button class="ts_toggle">✔︎ Close</button>
-        <small>v${version}</small>
+        <small>v${vers}</small>
         <small>
-          <a href="https://discord.gg/jnRs4WnPjM" target="_blank">Join me on the TT Discord</a>
+          <a href="https://discord.gg/jnRs4WnPjM" target="_blank">
+            Join me on the TT Discord
+          </a>
         </small>
       </div>
     </div>
