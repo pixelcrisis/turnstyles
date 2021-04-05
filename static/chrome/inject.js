773288bf1018cc9c37c9d51119367bee7e053fb7
12px <d@12px.io>
2021-04-05 16:15:30 -0600

    Add static files to build process.

---
+++ static/chrome/inject.js
@@ -0,0 +1,19 @@
+// inject our URL base into the window
+let base = chrome.runtime.getURL('themes/dark.css')
+const urlBase = document.createElement('script')
+urlBase.textContent = `window.tsBase = "${base.split('/themes')[0]}"`
+urlBase.type = "text/javascript"
+document.body.append(urlBase)
+
+// inject the main turnStyles script
+const turnStyles = document.createElement('script')
+turnStyles.type = "text/javascript"
+turnStyles.src = chrome.runtime.getURL('turnStyles.js')
+document.body.append(turnStyles)
+console.info(`turnStyles :: injected`)
+
+// pass a notification message to notify.js
+window.addEventListener("message", msg => {
+  if (event.data.type != "tsNotify") return
+  chrome.runtime.sendMessage('', event.data)
+})
