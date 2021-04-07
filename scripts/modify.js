cbe4777554780d0b2437d8e3a423123f2b9a1cb4
12px <d@12px.io>
2021-04-07 09:57:46 -0600

    Restructure modify/hidden.

--- scripts/modify.js
+++ scripts/modify.js
@@ -3,25 +3,24 @@
 module.exports = tS => {
 
   tS.modify = function () {
-    // bind song count updates
-    $('#songs-wrapper').on(event, '#songs', countSongs)
+    // song counter
+    const list = '#songs'
+    const song = '#songs-wrapper'
+    $(song).on(modified, list, countSongs)
     countSongs()
 
-    // bind profile stat link updates
-    $('#maindiv').on(event, '.overlay', attachStats)
+    // ttStats link
+    const main = '#maindiv'
+    const user = '.overlay .profile'
+    $(main).on(modified, user, attachStats)
 
-    // hide the audience/video
-    this.toggleClass('ts_hide_videos', this.config.no_vid)
-    this.toggleClass('ts_hide_audience', this.config.no_aud)
+    // hide elements
+    this.hidden('no_vid', this.config.no_vid)
+    this.hidden('no_aud', this.config.no_aud)
     
     // replace upload with organize
     $('#upload-button').after(`<div id="ts_upload"></div>`)
-    $('#ts_upload').on('click', () => {
-      let playlist = window.turntable.playlist
-      if (playlist.isFiltering) playlist.clearSearchBar()
-      $("#queue-header").removeClass("normal").addClass("edit")
-      playlist.queue.batchEditMode()
-    })
+    $('#ts_upload').on('click', orderSongs)
   }
 
   tS.hidden = function (key, value) {
@@ -34,7 +33,14 @@ module.exports = tS => {
 
 }
 
-const event = 'DOMSubtreeModified'
+const modified = 'DOMSubtreeModified'
+
+const orderSongs = () => {
+  let playlist = window.turntable.playlist
+  if (playlist.isFiltering) playlist.clearSearchBar()
+  $("#queue-header").removeClass("normal").addClass("edit")
+  playlist.queue.batchEditMode()
+}
 
 const countSongs = () => {
   let head = $('#playlist-header .text')[0]
@@ -44,14 +50,11 @@ const countSongs = () => {
 }
 
 const attachStats = () => {
-  // return if we've already attached
   if ($('.profile.modal .statslink').length) return
-
   // find the userid of the user
   let data = $('.profile.modal .userid')
   let find = data.length ? data[0].innerHTML : ''
   if (find.length != 24) return // not a valid id yet
-
   $('.profile.modal .section.web-links').before(layout(find))
 }
 
