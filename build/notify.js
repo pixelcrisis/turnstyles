85770bf0f2088ae733d67c074c07e5f67136eb49
12px <d@12px.io>
2021-04-24 19:17:18 -0600

    Build v5.7.0

--- build/notify.js
+++
@@ -1,9 +0,0 @@
-chrome.runtime.onMessage.addListener(data => {
-  if (data.type != "tsNotify" || !data.notification) return
-  chrome.notifications.create('', {
-    type: "basic",
-    title: data.notification.head,
-    message: data.notification.text,
-    iconUrl: "/images/icon128.png"
-  })
-})
