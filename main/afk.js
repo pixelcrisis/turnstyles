97185809281bc13daa0fc667538d5aa9d93c509c
12px <d@12px.io>
2022-01-17 09:06:39 -0700

    Add multi-message support to AFK response.

--- main/afk.js
+++ main/afk.js
@@ -5,24 +5,24 @@ module.exports = app => {
   // set afk status on update
   app.setAfk = function (key, val, grp) {
     if (key != 'is_afk' || grp) return
-    // send our afk ping if enabled
-    let msg = this.config.afkstr
-    if (val && msg) this.$Send(msg)
+    if (val && this.config.afkstr) this.isAfk()
   }
 
   // handle user being afk/active
   app.getAfk = function (e) {
-    let { is_afk, afkstr } = this.config
-    if (!is_afk || !afkstr) return
+    if (!this.config.is_afk) return
+    if (!e.$self && e.$ping) this.isAfk()
+  }
 
-    if (!e.$self && e.$ping) this.$Send(afkstr)
-    else if (e.$self && e.text != afkstr) {
-      this.setConfig('is_afk', false)
-      this.$Post({
-        head: `Welcome Back!`,
-        body: `You're no longer AFK!`
-      })
-    }
+  // end out afk ping
+  app.isAfk = function () {
+    if (!this.config.afkstr) return
+    let text = this.config.afkstr.split(';;')
+    if (text.length > 3) return this.$Post({
+      head: "AFK Error!",
+      body: `Contains ${text.length}/3 messages`
+    })
+    else for (let msg of text) this.$Send(msg.trim())
   }
 
   app.on('speak', app.getAfk)
