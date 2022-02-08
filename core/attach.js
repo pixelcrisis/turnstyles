e8f96291cda2a753acfb7c3eadc3b7b7f2d47e34
12px <d@12px.io>
2022-02-08 15:26:52 -0700

    Cleanup attach error catch.

--- core/attach.js
+++ core/attach.js
@@ -1,25 +1,26 @@
 // attach.js | connect app to the turntable room
 
-let error = "Oops! Something went wrong with turnStyles - if this is a bookmarklet, please go to https://ts.pixelcrisis.co to update your bookmarklet!"
-let check = () => {
-	if (this.__base) return true
-	if (window.confirm(error)) {
-		window.open('https://ts.pixelcrisis.co/', '_blank')
-	}
-	return false
-}
+let ts_url = 'https://ts.pixelcrisis.co'
+let issues = `Oops! Something went wrong with turnStyles! 
+If this is a bookmarklet, you may need to update it.
+To update, view the ts website at ${ ts_url } 
+Clicking OK will attempt to open the turnStyles website in a new tab.`
+let update = () => window.open(ts_url, '_blank')
 
 module.exports = app => {
 
 	// attach (init) into the turntable room
 	app.attach = function () {
-		// Make sure we've been injected
-		if (!check()) return false
-
 		this.getConfig()
 		const core = window.turntable
 		if (!core) return this.Log('no room')
 		const user = window.turntable.user
+
+		// Make sure we've been injected
+		if (!this.__base) {
+			if (window.confirm(issues)) update()
+			else return false
+		}
 
 		// we can't attach if we're in the lobby
 		this.lobby = $('#turntable #topBG').length
