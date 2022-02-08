374090693a9320adc604acc3ee44109b42421aa4
12px <d@12px.io>
2022-02-08 12:14:25 -0700

    Prepare to add separate bookmarklet entry points.

---
+++ browser.js
@@ -0,0 +1,13 @@
+// Bookmarklet Access To Plugin
+
+const path = "https://ts.pixelcrisis.co"
+const base = document.createElement('script')
+base.textContent = `window.tsBase = "${path}"`
+base.type = 'text/javascript'
+document.body.append(base)
+
+// inject the main turnStyles script
+const main = document.createElement('script')
+main.type = 'text/javascript'
+main.src = `${ path }/turnStyles.js?v=${ Math.random() }`
+document.body.append(main)
