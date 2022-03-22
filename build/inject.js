afad3aea756cfce3a8578faff77eb9b7bf538a5e
12px <d@12px.io>
2022-03-22 08:58:00 -0600

    Build v10.5.0

--- build/inject.js
+++ build/inject.js
@@ -1,16 +1,38 @@
 const ext = chrome || browser
-const url = ext.runtime.getURL('turnStyles.js')
+const url = ext.runtime.getURL("turnStyles.js")
 
-// inject url base into the window
-// this tells the script we're an extension
-const path = url.split('/turnStyles.js')[0]
-const base = document.createElement('script')
-base.textContent = `window.tsBase = "${path}"`
-base.type = 'text/javascript'
-document.body.append(base)
+const inject = () => {
+	// inject url base into the window
+	// this tells the script we're an extension
+	const path = url.split("/turnStyles.js")[0]
+	const base = document.createElement("script")
+	base.textContent = `window.tsBase = "${path}"`
+	base.type = "text/javascript"
+	document.body.append(base)
 
-// inject the main turnStyles script
-const main = document.createElement('script')
-main.type = 'text/javascript'
-main.src = url
-document.body.append(main)
+	// inject the main turnStyles script
+	const main = document.createElement("script")
+	main.type = "text/javascript"
+	main.src = url
+	document.body.append(main)
+}
+
+const sync = () => {
+	let local = window.localStorage.getItem("tsdb")
+	if (local) ext.storage.sync.set({ tsdb: JSON.parse(local) })
+	ext.storage.sync.get([ "tsdb" ], result => {
+		let store = result.tsdb ? JSON.stringify(result.tsdb) : ""
+		const data = document.createElement("script")
+		data.textContent = `window.tsSync = ${ store }`
+		data.type = "text/javascript"
+		document.body.append(data)
+		inject()
+	})
+}
+
+const init = () => {
+	if (!ext.storage) inject()
+	else sync()
+}
+
+init()
