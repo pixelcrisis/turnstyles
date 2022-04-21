42a55921a03dca9981d9e1260bce70dd6368b7b9
12px <d@12px.io>
2022-04-21 13:32:53 -0600

    Apply New Build Number Before Building Scripts.

--- bash/build.sh
+++ bash/build.sh
@@ -5,6 +5,17 @@ TEXT='\033[0;33m'
 
 # Added Some Color
 
+if [ $# -eq 1 ] 
+then
+	echo "${HEAD}Updating Versions"
+	echo "${TEXT}Updating ${CODE}package.json"
+	jq ".version = \"$1\"" package.json > package.temp.json
+	mv package.temp.json package.json
+	echo "${TEXT}Updating ${CODE}chrome/manifest.json"
+	jq ".version = \"$1\"" chrome/manifest.json > manifest.temp.json
+	mv manifest.temp.json chrome/manifest.json
+fi
+
 echo "${TEXT}Compiling ${CODE}turnStyles.js"
 browserify -p tinyify turnStyles.js -o build/turnStyles.js
 
@@ -19,17 +30,6 @@ postcss build/themes/*.css --use autoprefixer --d build/themes/
 echo "${TEXT}Compiling ${CODE}styles/"
 node-sass styles -o build/styles > /dev/null
 postcss build/styles/*.css --use autoprefixer --d build/styles/
-
-if [ $# -eq 1 ] 
-then
-	echo "${HEAD}Updating Versions"
-	echo "${TEXT}Updating ${CODE}package.json"
-	jq ".version = \"$1\"" package.json > package.temp.json
-	mv package.temp.json package.json
-	echo "${TEXT}Updating ${CODE}chrome/manifest.json"
-	jq ".version = \"$1\"" chrome/manifest.json > manifest.temp.json
-	mv manifest.temp.json chrome/manifest.json
-fi
 
 echo "${TEXT}Migrating ${CODE}chrome/"
 cp -R chrome/. build/
