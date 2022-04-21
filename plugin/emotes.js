833b58262ccb2a770c6a7f4381c768cc89f22a06
12px <d@12px.io>
2022-04-21 09:45:03 -0600

    Fix Current Song Marked Recent Add External Emoji Suggestions

--- plugin/emotes.js
+++ plugin/emotes.js
@@ -27,13 +27,19 @@ module.exports = App => {
 	App.parseBttv = function (name) {
 		let icon = this.bttvIcons[name]
 		let base = "https://cdn.betterttv.net/emote"
-		return `<img src="${ base }/${ icon }/1x">`
+		return icon ? `<img src="${ base }/${ icon }/1x">` : false
 	}
 
 	App.parseTwitch = function (name) {
 		let icon = this.twitchIcons[name]
 		let base = "https://static-cdn.jtvnw.net/emoticons/v2"
-		return `<img src="${ base }/${ icon }/static/light/1.0">`
+		return icon ? `<img src="${ base }/${ icon }/static/light/1.0">` : false
+	}
+
+	App.parseEmote = function (name) {
+		let icon = this.parseTwitch(name)
+		if (!icon) icon = this.parseBttv(name)
+		return icon
 	}
 
 	App.renderEmotes = function (emotes, list) {
@@ -50,10 +56,52 @@ module.exports = App => {
 		})
 	}
 
+	App.typeEmotes = function (e) {
+		if (!this.config.emojis) return
+		let text = e.target.value.split(" ")
+		let word = text[text.length - 1]
+		let test = word.length > 1 && word[0] === ":"
+
+		if (test) this.addTyping(word)
+		else $("#tsTyping").remove()
+	}
+
+	App.addTyping = function (text) {
+		let icons = []
+		let query = text.split(":").join("")
+		let found = s => s.indexOf(query) === 0
+		let listA = Object.keys(this.twitchIcons)
+		let listB = Object.keys(this.bttvIcons)
+		for (let i of listA) if ( found(i) ) icons.push(i)
+		for (let i of listB) if ( found(i) ) icons.push(i)
+
+		$("#tsTyping").remove()
+		let make = this.parseEmote.bind(this)
+		let bind = this.selectTyped.bind(this)
+		let list = icons.sort().map(name => typeHTML(make, name)).join("")
+		let html = `<div id="tsTyping"><div class="wrap">${ list }</div></div>`
+		$("body").append( html )
+		$("#tsTyping .icon").on("click", bind)
+	}
+
+	App.selectTyped = function (e) {
+		let name = e.target.dataset.icon
+		let text = $("#chat-input").val().split(" ")
+		text[text.length - 1] = `${ name } `
+		$("#chat-input").val(text.join(" ")).focus()
+		$("#tsTyping").remove()
+	}
+
 	App.bindEmotes = function () {
 		this.Bind("speak", this.checkEmote)
+		$("#chat-input").on("input", this.typeEmotes.bind(this))
 	}
 
 }
+
+const typeHTML = (make, name) => `
+	<div class="icon" data-icon=":${ name }:">
+		${ make(name) } :${ name }:
+	</div>`
 
 const tongue = `<span title="stuck_out_tongue" class="emoji emoji-stuck_out_tongue emoji-small"></span>`
